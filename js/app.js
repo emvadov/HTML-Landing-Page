64b62d2050444dc73458fd80c039f1628e253d3e
Emil Avadov <emvadov@gmail.com>
2021-01-25 02:41:38 +0200

    Created HTML, CSS and JavaScript files

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+/* STICKY HEADER */
+
+let prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+
+let currentScrollPos = window.pageYOffset;
+
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector(".header").style.top = "0";
+  } else {
+    document.querySelector(".header").style.top = "-70px";
+  }
+  prevScrollpos = currentScrollPos;
+}
